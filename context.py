2668bb8f51ef45d8f40099f114f7167a293cab1a
volttam <volttam@gmail.com>
2025-12-18 11:21:01 +0200

    added settings

--- context.py
+++ context.py
@@ -1,10 +1,11 @@
 
 from db_manager import DBManager
-from logger import get_logger
-from settings.settings import
+from settings.settings import Settings
+from hashing.passwordHasherSelector import PasswordHasherSelector
 
 class Context:
     def __init__(self):
         self.db_manager = DBManager()
-        self.logger = get_logger("logger")
+        self.settings = Settings()
+        self.password_hasher_selector = PasswordHasherSelector()
 
--- settings/settings.py
+++ settings/settings.py
@@ -2,13 +2,13 @@
 from pydantic_settings import BaseSettings
 from pydantic import Field
 from typing import Literal
-from hashing.config import PasswordHasherConfig
+
 
 
 class Settings(BaseSettings):
-    password_hasher: Literal["argon2id", "bcrypt", "sha256"] = Field(
-        default="argon2id",
-        alias="PASSWORD_HASHER",
+    hashing_mechanism: Literal["argon2id", "bcrypt", "sha256"] = Field(
+        default="sha256",
+        alias="HASHING_MECHANISM",
     )
 
     class Config:
