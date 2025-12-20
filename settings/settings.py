08a17006b95eebb30622c65b1a729c5118b4ce23
volttam <volttam@gmail.com>
2025-12-20 11:13:40 +0200

    fixed hash mode

--- settings/settings.py
+++ settings/settings.py
@@ -3,15 +3,16 @@ from pydantic_settings import BaseSettings
 from pydantic import Field
 from typing import Literal
 from pathlib import Path
+from pydantic import Field
 
 BASE_DIR = Path(__file__).resolve().parent.parent  # project root
 
 class Settings(BaseSettings):
-    hashing_mechanism: Literal["argon2id", "bcrypt", "sha256"] = Field(
+    hash_mode: Literal["argon2id", "bcrypt", "sha256"] = Field(
         default="sha256",
         alias="HASHING_MECHANISM",
     )
-    group_seed = "0x039C76D"
+    seed_group: str = Field(default="0x039C76D")
 
     class Config:
         env_file = BASE_DIR / "config" / ".env"
